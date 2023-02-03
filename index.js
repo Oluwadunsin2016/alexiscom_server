eeeb2aa17aa379f919eb101ba7485e323b6ad4a9
Oluwadunsin <oluwadunsin2016@gmail.com>
2023-02-03 17:23:34 +0100

    I am testing something

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ console.log('Mongoose connected successfully');
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
-app.use('/customer',customerRouter)
+app.use('/',customerRouter)
 
 app.listen(PORT,()=>{
 console.log(`app is listening at port ${PORT}`);
